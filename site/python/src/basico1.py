356761d616385d192ecd60553256a32ef9faf0da
FranciscoAriel <vazquez.chavez.francisco.ariel@hotmail.com>
2021-08-12 12:35:37 -0500

    sitio actualizado

--- site/python/src/basico1.py
+++ site/python/src/basico1.py
@@ -6,11 +6,17 @@ import pandas as pd
 import numpy as np
 import os
 
-# ver el directorio de trabajo
+# cambiar y ver el directorio de trabajo
+os.chdir("C:\\Users\\Francisco\\FranciscoAriel.github.io\\site\\python\\src")
 root=os.getcwd()
+print(root)
 
 # Leer archivos en csv (mismo directorio)
 datos = pd.read_csv("census.csv")
+
+# ver la info del archivo
+datos.info()
+datos.describe()
 
 # Mostrar las primeras 5 observaciones
 datos.head(n=5)
